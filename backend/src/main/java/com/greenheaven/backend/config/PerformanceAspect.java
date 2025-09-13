dfbb56b8da972b36c810c197dbea50688ba43712
rlawjggns <rlawjggns12@gmail.com>
2025-09-13 15:05:09 +0900

    기존 AOP에 유휴 커넥션 확인로그 추가

--- backend/src/main/java/com/greenheaven/backend/config/PerformanceAspect.java
+++ backend/src/main/java/com/greenheaven/backend/config/PerformanceAspect.java
@@ -1,5 +1,7 @@
 package com.greenheaven.backend.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,12 +10,20 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class PerformanceAspect {
+
+    private final HikariDataSource hikariDataSource;
 
     @Around("execution(* com.greenheaven.backend.service.*.*(..))") // service 패키지의 모든 메서드에 적용
     public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
+
+        // 💡 메서드 실행 전 커넥션 풀 상태 확인
+        log.info("메서드 실행 전 - 활성화된 커넥션: {}개, 유휴 커넥션: {}개",
+                hikariDataSource.getHikariPoolMXBean().getActiveConnections(),
+                hikariDataSource.getHikariPoolMXBean().getIdleConnections());
 
         Object result = joinPoint.proceed(); // 원본 메서드 실행
 
@@ -22,6 +32,12 @@ public class PerformanceAspect {
 
         String methodName = joinPoint.getSignature().toShortString();
         log.info("{}의 소요 시간: {}ms", methodName, duration);
+
+        // 💡 메서드 실행 후 커넥션 풀 상태 확인
+        log.info("메서드 실행 후 - 활성화된 커넥션: {}개, 유휴 커넥션: {}개",
+                hikariDataSource.getHikariPoolMXBean().getActiveConnections(),
+                hikariDataSource.getHikariPoolMXBean().getIdleConnections());
+
         return result;
     }
 }
